4eb2b4c73554aa3e0cb095a15f6a7051bbc046ed
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2024-09-14 21:41:50 +0600

    Start button sound effect added

--- utils/game/startPoint.js
+++ utils/game/startPoint.js
@@ -1,8 +1,10 @@
 import { startBtn, startPointContainer, tossContainer } from "../elements.js";
+import { clickSound } from "../sounds.js";
 import { gameValues } from "../variables.js";
 
 const gameStart = () => {
   startBtn.addEventListener("click", () => {
+    clickSound();
     startPointContainer.style.display = "none";
     gameValues.startPage = true;
     tossContainer.style.display = "flex";
